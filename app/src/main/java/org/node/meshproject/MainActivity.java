bd3a490d0f273826982f2d2dd41fce5e097893e8
Jonas Meeuws <jonas.meeuws@student.vives.be>
2017-11-27 12:27:50 +0100

    Updated share url

--- app/src/main/java/org/node/meshproject/MainActivity.java
+++ app/src/main/java/org/node/meshproject/MainActivity.java
@@ -93,6 +93,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         } else if (id == R.id.nav_share) {
             //TODO: Share app executable or it's link
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Reavershark/meshdevice-android/releases"));
+            startActivity(browserIntent);
         } else if (id == R.id.nav_feedback) {
             //TODO: Proper link
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Reavershark/meshdevice-android"));
